76766391d1ff555bd0f6f3ed559fd2dc13920692
Berik <eraliev237@gmail.com>
2023-09-16 00:54:44 +0600

    Cards | Project 1 | Bonus

--- marathon-urok-1/app.js
+++ marathon-urok-1/app.js
@@ -1,16 +1,20 @@
 "use strict";
 
-const slides = document.querySelectorAll(".slide");
+function slidesPlugin(activeSlide = 0) {
+	const slides = document.querySelectorAll(".slide");
 
-for (const slide of slides) {
-	slide.addEventListener("click", () => {
-		clearActiveSlides();
-		slide.classList.add("active");
-	});
+	for (const slide of slides) {
+		slide.addEventListener("click", () => {
+			clearActiveSlides();
+			slide.classList.add("active");
+		});
+	}
+
+	function clearActiveSlides() {
+		slides.forEach(slide => {
+			slide.classList.remove("active");
+		});
+	}
 }
 
-function clearActiveSlides() {
-	slides.forEach(slide => {
-		slide.classList.remove("active");
-	});
-}
+slidesPlugin(3);
